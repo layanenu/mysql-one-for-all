d0ca9cc98a6bf7e5fee298429753e85e3cd1475d
Layane Nunes <layane@MacBook-Air-de-Layane.local>
2023-01-02 12:02:28 -0300

    last commit

--- desafio6.sql
+++ desafio6.sql
@@ -1,10 +1,3 @@
---   SELECT 
--- 	  MIN(p.valor_plano) AS faturamento_minimo,
--- 	  MAX(p.valor_plano) AS faturamento_maximo,
---     ROUND(AVG(p.valor_plano),2) AS faturamento_medio,
---     ROUND(SUM(p.valor_plano),2) AS faturamento_total
--- FROM SpotifyClone.plano AS p
--- INNER JOIN SpotifyClone.usuario AS u ON u.plano_id = p.plano_id ;
 SELECT
   MIN(t1.valor_plano) AS faturamento_minimo,
   MAX(t1.valor_plano) AS faturamento_maximo,
